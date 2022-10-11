9e134c4930995d03201371c04bed037aa942c9fb
MojMohammadian <115070390+MojMohammadian@users.noreply.github.com>
2022-10-11 22:12:22 +0330

    Setup hilt module

---
+++ app/src/main/java/com/mohammadian/mrzpassportreader/di/AppModule.kt
@@ -0,0 +1,61 @@
+package com.mohammadian.mrzpassportreader.di
+
+import android.app.Application
+import androidx.camera.core.AspectRatio.RATIO_16_9
+import androidx.camera.core.CameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraSelector.LENS_FACING_BACK
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.FLASH_MODE_AUTO
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCameraSelector():CameraSelector{
+        return CameraSelector.Builder()
+            .requireLensFacing(LENS_FACING_BACK)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCameraProvider(application: Application): ProcessCameraProvider {
+        return ProcessCameraProvider.getInstance(application).get()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCameraPreview(): Preview{
+        return Preview.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageCapture(): ImageCapture{
+        return ImageCapture.Builder()
+            .setFlashMode(FLASH_MODE_AUTO)
+            .setTargetAspectRatio(RATIO_16_9)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageAnalysis(): ImageAnalysis{
+        return ImageAnalysis.Builder()
+            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+    }
+}
